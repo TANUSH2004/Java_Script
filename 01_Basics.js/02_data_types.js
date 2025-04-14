9209d3325bcb30d5bded5959d2e4d15bcabde8a0
TANUSH2004 <130989282+TANUSH2004@users.noreply.github.com>
2025-04-14 20:27:36 +0000

    data_types

---
+++ 01_Basics.js/02_data_types.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as of its newer version
+
+//alert(3+3); // this will not work because we are using node not browser
+
+//bigint
+//number
+//boolean
+//null => stand alone value
+//undefined => 
+//symbol => used for uniqueness.
+
+console.log(typeof "Tanush");// it is a string.
+console.log(typeof null); // null is a object in js
+console.log(typeof undefined); // it is undefined itself
+

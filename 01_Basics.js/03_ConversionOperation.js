5c0825727d0e748aa545a96b26ffae63fe1fccd4
TANUSH2004 <130989282+TANUSH2004@users.noreply.github.com>
2025-04-14 21:02:13 +0000

    operations

--- 01_Basics.js/03_ConversionOperation.js
+++ 01_Basics.js/03_ConversionOperation.js
@@ -5,3 +5,21 @@ let score = "88abc";
 let changed = Number(score);
 console.log(typeof changed);
 console.log(changed); // NaN (Not a Number is output because it is changed to Number but still there was abc in that string it cant be changed logically into a number but js doing it but when we need a output it will give NaN in such a case);
+
+
+// ******************operations***********
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Tanush";
+let str2 = "Chauhan";
+console.log(str1+str2);
+
+console.log(1 + "2");
+console.log(5+5+9+"000"); // num in starting treat as num then string
+console.log("3"+4+9+0); // string in start treat whole as string.

7fdb62408aa751e5bf29aa101504cf9a39b68f35
TANUSH2004 <130989282+TANUSH2004@users.noreply.github.com>
2025-04-14 20:12:35 +0000

    variables

---
+++ 01_Basics.js/01_variables.js
@@ -0,0 +1,9 @@
+const AccountId = 99;
+var name = "Tanush";
+let email= "tanush@gmail.com";
+hello = "world";
+
+//AccountId = 77; this is not allowed since it is a constant which cant be further moderated.
+
+console.log([AccountId,name,email,hello]);
+// prefer not to use var because it has scope problems.

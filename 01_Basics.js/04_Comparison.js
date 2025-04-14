dfe357778efa6dec8f054ff2da2fb0854fa65335
TANUSH2004 <130989282+TANUSH2004@users.noreply.github.com>
2025-04-14 21:25:30 +0000

    Comparisons

---
+++ 01_Basics.js/04_Comparison.js
@@ -0,0 +1,11 @@
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2<=1);
+
+console.log(null == 0); // == checks are different
+console.log(null >= 0);// others comparisons are different in background.
+console.log(null > 0);
+
+console.log("2" == 2);
+console.log("2" === 2); // === it gives a strict check and dont typecast automatically , otherwise it coerce it.

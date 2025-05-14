007e80365a49547473266d109c44ea31243ecbf5
TANUSH CHAUHAN <130989282+TANUSH2004@users.noreply.github.com>
2025-05-14 14:02:07 +0000

    scopes

---
+++ 03_baiscs.js/02_scope.js
@@ -0,0 +1,58 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5)) //we can access it even the function is defined later .
+
+function addone(num){
+    return num + 1
+}
+
+
+// while in this we cant access the function before declaring it because it is stored in variable and this is known as hoisting further will be discussed in later videos.
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
